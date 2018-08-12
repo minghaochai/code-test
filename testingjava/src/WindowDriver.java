c18bdf2ec730aace2b205034993f980316d1ba2e
ChaiMinghao <mcha159@student.monash.edu>
2018-08-12 15:10:51 +0800

    Hello World Java
    testing hello

---
+++ testingjava/src/WindowDriver.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class WindowDriver {
+    protected static Window w;
+    public static void main(String[] args){
+        String newString = "Hello World";
+        System.out.print(newString);
+    }
+}
